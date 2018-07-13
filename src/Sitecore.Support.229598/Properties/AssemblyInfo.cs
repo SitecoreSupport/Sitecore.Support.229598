de06ca44ffbe17d0a45838985ac47cc4f7b24712
DK\psk <psk@sitecore.net>
2018-07-13 20:19:25 +0300

    Fix typos

--- src/Sitecore.Support.229598/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.229598/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.229598")]
 [assembly: AssemblyProduct("Sitecore.Support.229598")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyDescription("Optimization for SearchService to run schema syncronization only after previous schema syncronization completed")]
+[assembly: AssemblyDescription("Optimization for SearchService to run schema synchronization only after previous schema synchronization completes")]
 
